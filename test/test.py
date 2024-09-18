e074c574e762fdf86cef1e53e8917d7d51dea8b4
Changshiao <3103407738@qq.com>
2024-09-18 08:39:38 +0800

    build_split

--- test/test.py
+++ test/test.py
@@ -1,1 +1,13 @@
-print('hello')
+import numpy as np
+
+X = np.array([
+    [1,3,4],
+    [2,4,5],
+    [3,5,6]
+])
+
+y = np.array([0,1,1])
+
+threshold = 2
+left_indices = X[:,0] < threshold
+print(X[left_indices][0])
